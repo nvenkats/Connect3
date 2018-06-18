de9ddf64252cc7e83247c5df10adf103a5a1a89b
nvenkats <nvenki505@gmail.com>
2018-06-18 13:12:53 +0530

    Made sure colour of the tile changes after each turn. Added tags to each ImageView

--- app/src/main/java/com/example/venkat/connect3/MainActivity.java
+++ app/src/main/java/com/example/venkat/connect3/MainActivity.java
@@ -7,11 +7,24 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //0 = yellow, 1 = red, 2 = unplayed
+
+    int activePlayer = 0;
+
+    int gameState[] = {2, 2, 2, 2, 2, 2, 2, 2, 2};
 
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
         counter.setTranslationY(-1000f);
-        counter.setImageResource(R.drawable.yellow);
+
+        if(activePlayer==0) {
+            counter.setImageResource(R.drawable.yellow);
+            activePlayer = 1;
+        }
+        else {
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+        }
         counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
     }
 
